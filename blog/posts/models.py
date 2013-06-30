373f2c413281ba0f60ea2c35bcbc37b9be2a6af0
pipado <pipado@pipado>
2013-06-30 17:03:49 +0100

    add a simple post model

---
+++ blog/posts/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+class Post(models.Model):
+    """
+    An item created by a user.
+    """
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, help_text=_(
+        "If omitted, the description will be determined by the first bit of the article's content."))
+    is_active = models.BooleanField(default=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.do_unique_slug()
+        super(Post, self).save(*args, **kwargs)
+
+    def do_unique_slug(self):
+        """
+        Ensures that the slug is always unique for this post
+        """
+        if not self.id:
+            # make sure we have a slug first
+            if not len(self.slug.strip()):
+                self.slug = slugify(self.title)
+
+            self.slug = self.get_unique_slug(self.slug)
+            return True
+
+        return False
+
+    def get_unique_slug(self, slug):
+        """
+        Iterates until a unique slug is found
+        """
+        orig_slug = slug
+        counter = 1
+
+        while True:
+            posts = Post.objects.filter(slug=slug)
+            if not posts.exists():
+                return slug
+
+            slug = '%s-%s' % (orig_slug, counter)
+            counter += 1
