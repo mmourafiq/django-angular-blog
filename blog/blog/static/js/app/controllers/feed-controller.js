27d4776e779c1cb157a45b2d5a10f70d2e4c821f
mourad <mouradmourafiq@gmail.com>
2013-07-10 17:46:43 +0100

    organize feed of posts, add create button with modal

--- blog/blog/static/js/app/controllers/feed-controller.js
+++ blog/blog/static/js/app/controllers/feed-controller.js
@@ -1,4 +1,32 @@
-Blog.controller('FeedController', function ($scope, GlobalService, posts) {
+Blog.controller('FeedController', function ($scope, GlobalService, PostService, posts) {
     $scope.posts = posts;
     $scope.globals = GlobalService;
+    //options for modals
+    $scope.opts = {
+        backdropFade: true,
+        dialogFade: true
+    };
+    //open modals
+    $scope.open = function (action) {
+        if (action === 'create'){
+            $scope.postModalCreate = true;
+            $scope.post = new Object();
+        };
+    };
+    //close modals
+    $scope.close = function (action) {
+        if (action === 'create'){
+            $scope.postModalCreate = false;
+        };
+    };
+    //calling board service
+    $scope.create = function () {
+        PostService.save($scope.post).then(function (data) {
+            $scope.post = data;
+            $scope.posts.push(data);
+            $scope.postModalCreate = false;
+        }, function(status){
+            console.log(status);
+        });
+    };
 });
