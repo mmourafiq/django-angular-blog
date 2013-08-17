d5a33c6ba79506f83d20d01932c648f03c50247d
mourad <mouradmourafiq@gmail.com>
2013-08-17 17:34:16 +0100

    add api-auth, redirect unautheticated users

--- blog/blog/dev_settings.py
+++ blog/blog/dev_settings.py
@@ -28,5 +28,8 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
